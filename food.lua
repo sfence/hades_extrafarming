584ab76fb56c45253fc18db0ae5f8f3b17934b52
tenplus1 <kinsellaja@yahoo.com>
2021-03-11 12:07:53 +0000

    add vegan cheese and recipe

--- food.lua
+++ food.lua
@@ -557,3 +557,42 @@ minetest.register_craft({
 	},
 	replacements = {{"mobs:bucket_milk", "bucket:bucket_empty"}}
 })
+
+-- Vegan Cheese
+
+minetest.register_craftitem("farming:cheese_vegan", {
+	description = S("Vegan Cheese"),
+	inventory_image = "farming_cheese_vegan.png",
+	on_use = minetest.item_eat(2),
+	groups = {food_cheese = 1, flammable = 2},
+})
+
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:cheese_vegan",
+	recipe = {
+		"farming:soy_milk", "farming:soy_milk", "farming:soy_milk",
+		"group:food_salt", "group:food_peppercorn", "farming:bottle_ethanol",
+		"group:food_gelatin", "group:food_pot"
+	},
+	replacements = {
+		{"farming:soy_milk", "vessels:drinking_glass 3"},
+		{"farming:pot", "farming:pot"},
+		{"farming:bottle_ethanol", "vessels:glass_bottle"}
+	}
+})
+
+minetest.register_craft({
+	type = "shapeless",
+	output = "farming:cheese_vegan",
+	recipe = {
+		"farming:soy_milk", "farming:soy_milk", "farming:soy_milk",
+		"group:food_salt", "group:food_peppercorn", "group:food_lemon",
+		"group:food_gelatin", "group:food_pot"
+	},
+	replacements = {
+		{"farming:soy_milk", "vessels:drinking_glass 3"},
+		{"farming:pot", "farming:pot"},
+	}
+})
